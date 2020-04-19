5ea12caaecedd6dfb954c023b3a463973cdcb7b7
Carlo Janea <janeacarlo@gmail.com>
2020-04-19 10:16:08 +0800

    added fixes and comments{

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const App = () => {
-  return <div>Personal Config!!</div>;
-};
-
-ReactDOM.render(<App />, document.getElementById("root"));
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// hot-loaded by babel
+// fork-ts-webpack-plugin
 const App = () => {
-  return <div>Personal Configss!!</div>;
+  return <div>Personal Config!</div>;
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
